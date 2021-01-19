efd522fd86796e3a3f5ec7e582cea035f90e5e83
rphilips <richard.philips@gmail.com>
2021-01-19 14:40:27 +0100

    Implemented touch on files

---
+++ brocade.be/qtechng/cli/cmd/fs_touch.go
@@ -0,0 +1,122 @@
+package cmd
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+	"time"
+
+	qparallel "brocade.be/base/parallel"
+	qerror "brocade.be/qtechng/lib/error"
+	"github.com/spf13/cobra"
+)
+
+var fsTouchCmd = &cobra.Command{
+	Use:   "touch",
+	Short: "Touches files",
+	Long: `The last modification time of all files is changed to the current moment.
+If the argument is a directory name, all files in that directory are handled.`,
+	Args:    cobra.MinimumNArgs(0),
+	Example: `qtechng fs touch cwd=../catalografie`,
+	RunE:    fsTouch,
+	Annotations: map[string]string{
+		"remote-allowed": "no",
+	},
+}
+
+func init() {
+	fsTouchCmd.Flags().BoolVar(&Frecurse, "recurse", false, "Recurse directories")
+	fsTouchCmd.Flags().BoolVar(&Ftolower, "tolower", false, "Lowercase before grepping")
+	fsTouchCmd.Flags().StringSliceVar(&Fpattern, "pattern", []string{}, "Posix glob pattern on the basenames")
+	fsCmd.AddCommand(fsTouchCmd)
+}
+
+func fsTouch(cmd *cobra.Command, args []string) error {
+	reader := bufio.NewReader(os.Stdin)
+
+	ask := false
+	if len(args) == 0 {
+		ask = true
+		for {
+			fmt.Print("File/directory        : ")
+			text, _ := reader.ReadString('\n')
+			text = strings.TrimSuffix(text, "\n")
+			if text == "" {
+				break
+			}
+			args = append(args, text)
+		}
+		if len(args) == 0 {
+			return nil
+		}
+	}
+
+	if ask && !Frecurse {
+		fmt.Print("Recurse ?               : <n>")
+		text, _ := reader.ReadString('\n')
+		text = strings.TrimSuffix(text, "\n")
+		if text == "" {
+			text = "n"
+		}
+		if strings.ContainsAny(text, "jJyY1tT") {
+			Frecurse = true
+		}
+	}
+
+	if ask && len(Fpattern) == 0 {
+		for {
+			fmt.Print("Pattern on basename     : ")
+			text, _ := reader.ReadString('\n')
+			text = strings.TrimSuffix(text, "\n")
+			if text == "" {
+				break
+			}
+			Fpattern = append(Fpattern, text)
+		}
+	}
+	files, err := glob(Fcwd, args[1:], Frecurse, Fpattern)
+
+	if len(files) == 0 {
+		if err != nil {
+			Fmsg = qerror.ShowResult("", Fjq, err)
+			return nil
+		}
+		msg := make(map[string][]string)
+		msg["files"] = files
+		Fmsg = qerror.ShowResult(msg, Fjq, nil)
+		return nil
+	}
+	h := time.Now().Local()
+	fn := func(n int) (interface{}, error) {
+		src := files[n]
+		et := os.Chtimes(src, h, h)
+		return src, et
+	}
+
+	resultlist, errorlist := qparallel.NMap(len(files), -1, fn)
+	var errs []error
+	var touched []string
+	for i := range resultlist {
+		src := files[i]
+		if errorlist[i] != nil {
+			e := &qerror.QError{
+				Ref:  []string{"fs.touch"},
+				File: src,
+				Msg:  []string{errorlist[i].Error()},
+			}
+			errs = append(errs, e)
+			continue
+		}
+		touched = append(touched, src)
+	}
+
+	msg := make(map[string][]string)
+	msg["touched"] = touched
+	if len(errs) == 0 {
+		Fmsg = qerror.ShowResult(msg, Fjq, nil)
+	} else {
+		Fmsg = qerror.ShowResult(msg, Fjq, qerror.ErrorSlice(errs))
+	}
+	return nil
+}
