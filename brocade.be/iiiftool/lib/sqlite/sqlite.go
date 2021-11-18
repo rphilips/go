1d466a88640a4841cb80f81002135686b60e7666
Tom Deneire <tomdeneire@gmail.com>
2021-11-18 09:32:09 +0100

    checks for file store

--- brocade.be/iiiftool/lib/sqlite/sqlite.go
+++ brocade.be/iiiftool/lib/sqlite/sqlite.go
@@ -1,6 +1,10 @@
 package sqlite
 
 import (
+	"errors"
+	"fmt"
+
+	fs "brocade.be/base/fs"
 	identifier "brocade.be/iiiftool/lib/identifier"
 )
 
@@ -8,7 +12,20 @@ import (
 // store the files in the appropriate SQLite archive
 func Store(id identifier.Identifier, files []string) error {
 	path := id.Location()
-	// is path valid?
+
+	if fs.Exists(path) {
+		return errors.New("location already has data")
+		// to do: provide append mode?
+	}
+	for _, file := range files {
+		if !fs.IsFile(file) {
+			return errors.New("file is not valid:\n" + file)
+		}
+	}
+
+	fmt.Println("okay")
+
+	// store files at location
 
 	return nil
 }
