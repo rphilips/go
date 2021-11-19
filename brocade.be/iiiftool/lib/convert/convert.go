ca87e57eda03fc6737c93a11a6b646b3247e96a7
Tom Deneire <tomdeneire@gmail.com>
2021-11-19 08:44:01 +0100

    convert parallel

--- brocade.be/iiiftool/lib/convert/convert.go
+++ brocade.be/iiiftool/lib/convert/convert.go
@@ -2,22 +2,26 @@ package convert
 
 import (
 	"fmt"
+	"sync"
 )
 
-func convert(file string, err chan error) {
-	err <- nil
+func convert(file string, wg *sync.WaitGroup, errors *[]error) {
+	defer wg.Done()
+	// converteer de files en geef errors terug
+	*errors = append(*errors, fmt.Errorf(file+": error"))
 }
 
 // Wrapper for file conversion
 func Run(files []string) error {
+	var errors []error
+	var wg sync.WaitGroup
 	for _, file := range files {
-		convErr := make(chan error)
-		go convert(file, convErr)
-		fmt.Println(file)
-		err := <-convErr
-		if err != nil {
-			return fmt.Errorf("error converting file %s: %v", file, err)
-		}
+		wg.Add(1)
+		go convert(file, &wg, &errors)
+	}
+	wg.Wait()
+	if len(errors) > 0 {
+		return fmt.Errorf("error converting file(s) %s", errors)
 	}
 	return nil
 }
