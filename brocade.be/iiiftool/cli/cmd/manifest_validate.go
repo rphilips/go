1a5914fd6bdec437aa5ee4e63306fa05c9cee4a3
Tom Deneire <tomdeneire@gmail.com>
2022-01-27 08:46:21 +0100

    manifest validate with digest

--- brocade.be/iiiftool/cli/cmd/manifest_validate.go
+++ brocade.be/iiiftool/cli/cmd/manifest_validate.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"fmt"
 	"log"
+	"strings"
 
 	"brocade.be/iiiftool/lib/iiif"
 	"brocade.be/qtechng/lib/report"
@@ -12,11 +13,13 @@ import (
 var manifestValidateCmd = &cobra.Command{
 	Use:   "validate",
 	Short: "Validate manifest for a IIIF identifier",
-	Long: `Validate a IIIF manifest URL given a certain version (default: 3.0)
-	against https://presentation-validator.iiif.io/`,
-	Args:    cobra.ExactArgs(1),
-	Example: `iiiftool manifest validate https://dev.anet.be/iiif/e0f4d5d32a3dd5a341ec84a2ae8e9c69e2666fca/manifest --version=2.1`,
-	RunE:    manifestValidate,
+	Long: `Validate a IIIF manifest given a certain version (default: 3.0)
+	against https://presentation-validator.iiif.io/.
+	The argument is either a IIIF manifest URL or a IIIF digest`,
+	Args: cobra.ExactArgs(1),
+	Example: `iiiftool manifest validate https://dev.anet.be/iiif/e0f4d5d32a3dd5a341ec84a2ae8e9c69e2666fca/manifest --version=2.1
+	iiiftool manifest validate e0f4d5d32a3dd5a341ec84a2ae8e9c69e2666fca`,
+	RunE: manifestValidate,
 }
 
 var Fversion string
@@ -27,9 +30,16 @@ func init() {
 }
 
 func manifestValidate(cmd *cobra.Command, args []string) error {
-	url := args[0]
-	if url == "" {
+	id := args[0]
+	if id == "" {
 		log.Fatalf("iiiftool ERROR: argument is empty")
+	}
+
+	url := ""
+	if strings.HasPrefix(id, "http") {
+		url = id
+	} else {
+		url = "https://dev.anet.be/iiif/" + id + "/manifest"
 	}
 
 	result, err := iiif.Validate(url, Fversion)
